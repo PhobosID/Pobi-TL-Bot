51f83f104aeeda89990c7b5cf32daf7780a194f9
Kristoff Liao <git.glaring773@passinbox.com>
2024-07-30 16:52:20 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Telegraf, Markup } = require('telegraf');
+const bot = new Telegraf('##########'); // Replace ########## With your Bot Token from BotFather
+
+bot.start((ctx) => { // Initialize Start Command (/start)
+  ctx.reply(
+    "Welcome! This is a Welcome Text for Initiating Telegram Bot",
+    Markup.inlineKeyboard([
+      [
+        { text: "URL Button", url: "https://example.phobos.id" },
+        { text: "Chat Button", callback_data: "foo" },
+        { text: "Another Button", callback_data: "bar" }
+      ]
+    ])
+  );
+});
+
+bot.on('callback_query', (ctx) => {
+  const data = ctx.callbackQuery.data;
+
+  if (data === "foo") {
+    ctx.reply('This is A Response Text');
+  } else if (data === "bar") {
+    ctx.reply('This is Another Text');
+  }
+});
+
+bot.help((ctx) => { // Initialize Help Command (/help)
+    ctx.reply(
+    "Here is the list of the commands.\n\n/start - Initialize the Bot\n/info - Information about the Bot"
+    )
+});
+
+bot.info((ctx) => { // Initialize Info Command (/info)
+    ctx.reply(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat tellus non nunc facilisis, vitae ultrices dolor varius."
+    )
+});
+
+bot.launch().then(() => {
+    console.log("Bot is Ready to Use!");
+});
